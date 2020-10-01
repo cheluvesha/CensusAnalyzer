a1ad6dc3b9345b73819d9e6dae37cf2c394ad78b
cheluvesha <cheluvesh.b@gmail.com>
2020-10-01 20:34:33 +0530

    [CHELUVESHA]Added : FilePath wrong exception

--- src/test/scala/com/cenusAnalyzerTest/CensusAnalyzerTest.scala
+++ src/test/scala/com/cenusAnalyzerTest/CensusAnalyzerTest.scala
@@ -2,12 +2,18 @@ package com.cenusAnalyzerTest
 
 import org.scalatest.FunSuite
 import com.censusAnalyzer.CensusAnalyzer
+import com.censusAnalyzer.CensusAnalyzerExceptionEnum
 
 class CensusAnalyzerTest extends FunSuite {
-
+  val CensusAnalyzerObj = new CensusAnalyzer()
   test("MatchingNumberOfRows_Input_CSVFileWithRightPath_ReturnNumberOfRows"){
-    val objCensus = new CensusAnalyzer()
-    assert(objCensus.loadCSVInfo("/home/cheluvesha/IdeaProjects/CensusAnalyzer/src/test/Resources/IndiaStateCensusData.csv") === 29)
+    assert(CensusAnalyzerObj.loadCSVInfo("/home/cheluvesha/IdeaProjects/CensusAnalyzer/src/test/Resources/IndiaStateCensusData.csv") === 29)
   }
 
+  test("IfFilePathIsWrong_ReturnsIncorrectFilePathException") {
+    val thrown = intercept[Exception] {
+      CensusAnalyzerObj.loadCSVInfo("./src/scala/CensusAnalyzerProject/Resources/IndiaStateCensusData.csv")
+    }
+    assert(thrown.getMessage === CensusAnalyzerExceptionEnum.InCorrectPath.toString)
+  }
 }
